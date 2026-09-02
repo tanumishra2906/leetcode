110c9e0927dcf92c8cd9ed504adacf6241e3fdd6
tanumishra2906 <25110cn561@glbitm.ac.in>
2026-09-03 01:03:49 +0530

    Time: 0 ms (100%), Space: 20.3 MB (83.3%) - LeetHub-Neo

---
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -0,0 +1,19 @@
+void moveZeroes(int* nums, int numsSize)
+{
+    int j=0;
+
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]!=0)
+        {
+            nums[j]=nums[i];
+            j++;
+        }
+    }
+
+    while(j<numsSize)
+    {
+        nums[j]=0;
+        j++;
+    }
+}
